7a5d38c2c278f0010e79df6c85dd33f7bb288d67
Peter C <ptrcortes@email.arizona.edu>
2015-03-06 21:19:10 -0700

    commit jetty jars

---
+++ src/textadrone/SimplestServer.java
@@ -0,0 +1,13 @@
+package textadrone;
+
+import org.eclipse.jetty.server.Server;
+
+public class SimplestServer
+{
+    public static void main(String[] args) throws Exception
+    {
+        Server server = new Server(8080);
+        server.start();
+        server.join();
+    }
+}
