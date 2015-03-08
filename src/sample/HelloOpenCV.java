524ac6e709ddfa12be01f956aabd18ba3214cb02
Peter C <ptrcortes@email.arizona.edu>
2015-03-08 07:51:02 -0700

    update opencv

--- src/sample/HelloOpenCV.java
+++ src/sample/HelloOpenCV.java
@@ -2,12 +2,14 @@ package sample;
 
 import org.opencv.core.Core;
 
-public class HelloOpenCV {
-	  public static void main(String[] args) {
-	    System.out.println("Hello, OpenCV");
+public class HelloOpenCV
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Hello, OpenCV");
 
-	    // Load the native library.
-	    System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-	    new DetectFaceDemo().run();
-	  }
+		// Load the native library.
+		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+		new DetectFaceDemo().run();
 	}
+}
